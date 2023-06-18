cc283fbfaa14ece1f5517dfa7c3f09ccb733ffc2
MiguelCordero-7 <mc7242921@gmail.com>
2023-06-18 16:04:33 -0600

    clase 2

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -1,17 +1,41 @@
 import random
+from game.utils.constants import SCREEN_WIDTH,SCREEN_HEIGHT, LEFT, RIGTH
 
 class Enemy:
     Y_POS = 0
     X_POS_LIST = [50,150, 250, 350, 450, 550, 650, 750, 850, 950]
-
+    SPEED_Y = 3
+    SPEED_X = 2
+    MOV_X = [LEFT, RIGTH]
+    INTERVAL = 100
+    
     def __init__(self, image):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = random.choice(self.X_POS_LIST)
         self.rect.y = self.Y_POS
+        self.mov_x = random.choice(self.MOV_X)
+        self.is_alive = True
+        self.index = 0
 
     def update(self):
-        pass
+        if self.rect.y >= SCREEN_HEIGHT:
+            self.is_alive = False
+        self.move()
 
     def draw (self, screen):
         screen.blit(self.image, self.rect)
+    
+    def move(self):
+        self.rect.y += self.SPEED_Y
+        if self.mov_x == LEFT:
+            self.rect.x -= self.SPEED_X
+            if self.index > self.INTERVAL or self.rect.left <= 0:
+                self.mov_x = RIGTH
+                self.index = 0
+        else: 
+            self.rect.x += self.SPEED_X
+            if self.index > self.INTERVAL or self.rect.right >= SCREEN_WIDTH:
+                self.mov_x = LEFT
+                self.index = 0
+        self.index += 1 
--- game/components/enemies/enemy_handler.py
+++ game/components/enemies/enemy_handler.py
@@ -8,12 +8,15 @@ class EnemyHandler:
         self.add_enemy()
         for enemy in self.enemies:
             enemy.update()
-
+            if not enemy.is_alive:
+                self.remove_enemy(enemy)
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
 
     def add_enemy(self):
-        self.enemies.append(Ship())
+        if len (self.enemies) < 4:
+            self.enemies.append(Ship())
 
-
+    def remove_enemy(self, enemy):
+        self.enemies.remove(enemy)
